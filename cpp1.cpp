f99d7bb37b07242370aaeff400cd9073aa13d3e3
Sina Jafari <168327662+sina0101@users.noreply.github.com>
2024-09-09 01:51:48 +0330

    Add files via upload

---
+++ cpp1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+#include<string>
+main()
+{
+   
+   string fname,lname;
+   int age;
+   cout <<"your name: ";
+   cin >>fname;
+
+   cout <<"your lname: ";
+   cin >>lname;
+
+
+   cout <<"your age: ";
+   cin >>age;
+
+
+   cout << "your name is "<<fname << " " << lname <<" and you are " << age << " years old ";
+   
+
+
+}
+ 
+//>   sourceiran.com
